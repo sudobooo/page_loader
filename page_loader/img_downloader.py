f3202f01993237f902bfec26bdd4eaef29115a90
sudobooo <marsdoghelena@gmail.com>
2022-05-24 00:19:14 +0300

    Update img_downloader and page_downloader

--- page_loader/img_downloader.py
+++ page_loader/img_downloader.py
@@ -1,47 +1,28 @@
 import os
 import requests
 
-from urllib.parse import urljoin, urlparse
 from page_loader.url_converter import convert
+from page_loader.content_checker import check_content
+from page_loader.writer import write_content
 
 
 def download_img(data, url, dir, path):
 
-    block = data.find_all('img')
+    TAGS_AND_ATTRIBUTES = {
+        'img': 'src',
+        'script': 'src',
+        'link': 'href',
+    }
 
-    for teg in block:
-        image = teg.get('src')
-        if image.startswith('http'):
-            if not same_netloc(image, url):
-                continue
-            image_link = image
-        else:
-            if not image.startswith('/'):
-                image = '/' + image
-            image_link = urljoin(url, image)
+    for teg in data.find_all(TAGS_AND_ATTRIBUTES.keys()):
+        attribute = TAGS_AND_ATTRIBUTES.get(teg.name)
+        content = teg.get(attribute)
+        content_link = check_content(url, content)
+        if not content_link:
+            continue
 
-        path_image = os.path.join(path,
-                                  convert(image_link))
-        teg['src'] = os.path.join(dir,
-                                  convert(image_link))
+        path_image = os.path.join(path, convert(content_link))
+        teg[attribute] = os.path.join(dir, convert(content_link))
 
-        image_response = requests.get(image_link).content
-        write_img(path_image, image_response)
-
-
-def write_html(path, content):
-    with open(path, "w", encoding="utf-8") as file:
-        file.write(content)
-
-
-def write_img(path, content):
-    with open(path, "wb") as file:
-        file.write(content)
-
-
-def same_netloc(first_url, second_url):
-
-    first_parse_link = urlparse(first_url)
-    second_parse_link = urlparse(second_url)
-
-    return first_parse_link.netloc == second_parse_link.netloc
+        image_response = requests.get(content_link).content
+        write_content(path_image, image_response)
--- page_loader/page_downloader.py
+++ page_loader/page_downloader.py
@@ -3,10 +3,12 @@ import os
 from bs4 import BeautifulSoup
 
 from page_loader.url_converter import convert
-from page_loader.img_downloader import download_img, write_html
+from page_loader.img_downloader import download_img
+from page_loader.writer import write_html
 
 
 def download(url, actual_path=os.getcwd()):
+
     response = requests.get(url).text
     dir = convert(url, 'dir')
     path_to_dir = os.path.join(actual_path, dir)
